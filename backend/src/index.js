e42def4230caeab91893a2c80547408deec8f944
Andre Ohuti <ohuti@hotmail.com>
2020-03-24 14:15:09 -0300

    Added Morgan for route logging

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const routes = require('./routes')
+const morgan = require('morgan')
 
 const app = express()
 
 app.use(cors())
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(routes)
 
